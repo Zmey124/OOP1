c4d8f0a9d332d4ad01411ec84ca54c77b2a2baab
Zmey <zmey12489@yandex.ru>
2024-11-13 18:47:54 +0700

    DZ1

--- DZ1OOP.py
+++ DZ1OOP.py
@@ -9,22 +9,21 @@ with open('recipes.txt', 'r', encoding='utf-8') as file:
     for line in file:
         line = line.strip()
 
-        if line == '':  # Empty line indicates end of recipe
+        if line == '': 
             if recipe_name:
                 cook_book[recipe_name] = ingredients
                 recipe_name = None
                 ingredients = []
 
-        elif recipe_name is None:  # First line of a new recipe
+        elif recipe_name is None: 
             recipe_name = line
-            num_steps = int(file.readline().strip())  # Read the number of steps (ignore it for now)
-            file.readline()  # Skip the empty line
+            num_steps = int(file.readline().strip()) 
+            file.readline() 
 
-        else:  # Ingredient line
+        else: 
             ingredient_name, quantity, unit = line.split(' | ')
             ingredients.append({'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': unit})
 
-# Handle the last recipe (no empty line after it)
 if recipe_name:
     cook_book[recipe_name] = ingredients
 
